24c1bd5bcfd39eee085d0856bfde01201f4d4237
amndalsr <laisamanda2015@gmail.com>
2024-07-19 13:34:08 -0300

    feat: add appointment routes

---
+++ routes/appointments.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { createAppointment, getAppointments } = require('../controllers/appointmentController');
+
+const router = express.Router();
+
+router.post('/', createAppointment);
+router.get('/', getAppointments);
+
+module.exports = router;
